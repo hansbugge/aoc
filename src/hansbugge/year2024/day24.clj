6e15cb89eaed044b6474ac2aa2109b4ab9395c78
Hans Bugge Grathwohl <hbg@globeteam.com>
2024-12-26 21:39:01 +0100

    solve day 24, part 2 - finally

--- src/hansbugge/year2024/day24.clj
+++ src/hansbugge/year2024/day24.clj
@@ -1,106 +1,203 @@
 (ns hansbugge.year2024.day24
   (:require
+   [clojure.pprint :as pprint]
    [clojure.string :as str]
    [hansbugge.utils :as utils]
    [ubergraph.alg :as ua]
-   [ubergraph.core :as ug]))
+   [ubergraph.core :as ug]
+   [medley.core :as m]))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 
 (defonce input (utils/fetch-input {:year 2024 :day 24}))
-(def test-input "x00: 1\nx01: 1\nx02: 1\ny00: 0\ny01: 1\ny02: 0\n\nx00 AND y00 -> z00\nx01 XOR y01 -> z01\nx02 OR y02 -> z02")
 
 (defn parse [input]
-  (let [[assignments constraints] (str/split input #"\n\n")]
-    {:assignments (into {} (comp (map #(str/split % #": "))
-                                 (map (juxt (comp symbol first) (comp parse-long second))))
-                        (str/split-lines assignments))
-     :constraints (->> (str/split-lines constraints)
-                       (map #(let [[_ v1 o v2 r] (re-matches #"(.{3}) (AND|OR|XOR) (.{3}) -> (.{3})" %)]
-                               (into [(keyword (str/lower-case o))] (map symbol) [v1 v2 r])))
-                       (into []))}))
+  (let [[init-vals gates] (str/split input #"\n\n")]
+    {:init-vals (into {} (comp (map #(str/split % #": "))
+                               (map (juxt first (comp parse-long second))))
+                      (str/split-lines init-vals))
+     :gates (->> (str/split-lines gates)
+                 (map #(let [[_ v1 o v2 r] (re-matches #"(.{3}) (AND|OR|XOR) (.{3}) -> (.{3})" %)]
+                         (into [(keyword (str/lower-case o))] [v1 v2 r])))
+                 (into []))}))
 
-;; build graph
 (defn ->graph [input]
-  (let [{:keys [assignments constraints]} (parse input)
-        g (apply ug/digraph
-                 [:hi {:val 1}]
-                 [:lo {:val 0}]
-                 constraints)
-        edges (into (for [[wire v] assignments]
-                      [(if (= 1 v) :hi :lo) wire])
+  (let [{:keys [init-vals gates]} (parse input)
+        edges (into (for [[wire v] init-vals]
+                      [[:init v]
+                       wire])
                     cat
-                    (for [[_ w1 w2 w3 :as n] constraints]
+                    (for [[_ w1 w2 w3 :as n] gates]
                       [[w1 n]
                        [w2 n]
                        [n w3]]))]
-    (apply ug/add-directed-edges g edges)))
-
-(comment
-  (-> (->graph test-input)
-      (ug/viz-graph {:auto-label true}))
-
-  (def *g (->graph input))
-  *g
-  (ug/viz-graph *g)
-  (-> (->graph input)
-      (ug/viz-graph {:auto-label false
-                     :save {:filename "graph.png" :format :png}}))
-  )
+    (apply ug/digraph edges)))
 
 (defn simulate [g]
-  (reduce
-   (fn [g node]
-     (cond
-       (keyword? node)
-       ;; hi or lo, do nothing
-       g
+  (when-let [s (ua/topsort g)]
+    (reduce
+     (fn [g node]
+       (cond
+         (string? node)
+         ;; wire, carry the value over from predecessor
+         (let [pred (first (ug/predecessors g node))
+               _ (assert pred "there should be one predecessor")]
+           (ug/add-attr g node :val (ug/attr g pred :val)))
 
-       (symbol? node)
-       ;; wire, carry the value over from predecessor
-       (let [pred (first (ug/predecessors g node))
-             _ (assert pred "there should be one predecessor")]
-         (ug/add-attr g node :val (ug/attr g pred :val)))
+         ;; init-node, set val to literal value
+         (= :init (first node))
+         (ug/add-attr g node :val (second node))
 
-       :else
-       ;; gate, there should be two predecessors
-       (let [preds (ug/predecessors g node)
-             _ (assert (= 2 (count preds)) "there should be exactly two predecessors")
-             op (case (first node)
-                  :and bit-and
-                  :or bit-or
-                  :xor bit-xor)
-             input (map #(ug/attr g % :val) preds)]
-         (ug/add-attr g node :val (apply op input)))))
-   g
-   (ua/topsort g)))
+         :else
+         ;; gate, there should be two predecessors
+         (let [preds (ug/predecessors g node)
+               _ (assert (= 2 (count preds)) "there should be exactly two predecessors")
+               op (case (first node)
+                    :and bit-and
+                    :or bit-or
+                    :xor bit-xor)
+               input (map #(ug/attr g % :val) preds)]
+           (ug/add-attr g node :val (apply op input)))))
+     g
+     s)))
 
-(comment
-  (-> (->graph test-input)
-      simulate
-      (ug/viz-graph {:auto-label true}))
+(defn get-binary-val [g var-name]
+  (->> g
+       :attrs
+       (filter #(str/starts-with? (key %) var-name))
+       (sort #(compare %2 %1))
+       (map (comp :val val))
+       (apply str)))
 
-  (-> (->graph input)
-      simulate
-      (ug/viz-graph {:auto-label true
-                     ;; :dot, :neato, :fdp, :sfdp, :twopi, or :circo
-                     :layout :dot
-                     :save {:filename "graph.png" :format :png}}))
-  )
+(defn to-binary ^String [^long n]
+  (Long/toBinaryString n))
+
+(defn from-binary ^long [^String b]
+  (BigInteger. b 2))
 
 (defn part-1 [input]
-  (let [g (simulate (->graph input))
-        binary-string (->> (ug/nodes g)
-                           (filter #(and (symbol? %)
-                                         (= \z (first (str %)))))
-                           (map (fn [n] [n (ug/attr g n :val)]))
-                           (sort #(compare %2 %1))
-                           (map second)
-                           (apply str))]
-    (Long/parseLong binary-string 2)))
+  (-> (->graph input)
+      simulate
+      (get-binary-val "z")
+      (from-binary)))
 
 (comment
   (time (part-1 input))
   ;; => 46362252142374
+  )
+
+(comment
+  ;;; Part 2: figure out which wires to swap.
+
+  ;;; Desired:
+  (let [g (simulate (->graph input))]
+    (-> (+ (from-binary (get-binary-val g "x"))
+           (from-binary (get-binary-val g "y")))
+        to-binary))
+  ;; => "1010011110101010001101100100100000011011100110"
+
+  ;;; Current:
+  (-> (->graph input)
+      simulate
+      (get-binary-val "z"))
+  ;; => "1010100010101010001101100100011110011100100110"
+
+
+  ;;; bits to change:
+  ;;; z06..z08 should be flipped (100 -> 011)
+  ;;; z13..z17 should be flipped (01111 -> 10000)
+  ;;; z38..z41 should be flipped (1000 -> 0111)
+
+  ;;; current vs. desired:
+  ;;;"1010 1000 10101010001101100100 01111 0011 100 100110"
+  ;;;"1010 0111 10101010001101100100 10000 0011 011 100110"
+  )
+
+(defn swap-output-wires [g w1 w2]
+  (let [nodes (ug/nodes g)
+        n1 (m/find-first #(= w1 (nth % 3 nil)) nodes)
+        n2 (m/find-first #(= w2 (nth % 3 nil)) nodes)]
+    (-> g
+        (ug/remove-edges [n1 w1]
+                         [n2 w2])
+        (ug/add-edges [n1 w2]
+                      [n2 w1]))))
+
+
+(def g (->graph input))
+
+(comment
+  (ug/viz-graph g {:save {:filename "graph.pdf" :format :pdf}})
+  )
+
+(comment
+  ;;; The input seems to be (almost) a ripple-carry adder.
+  ;;; It consists of adder components with 5 gates, taking as input
+  ;;; x(n), y(n), carry(n) and giving output z(n+1), carry(n+1).
+
+  ;;; adder component:
+  ;;;
+  ;;; for x01 and y01 you have
+  ;;; x01   AND y01      -> and01
+  ;;; x01   XOR y01      -> xor01
+  ;;; cin01 XOR xor01    -> z01
+  ;;; cin01 AND xor01    -> andxor01
+  ;;; and01 OR  andxor01 -> cin02
+
+  ;;; c01 = x00 AND y00 = nqp
+  ;;; z00 = x00 XOR y00
+
+  ;;; we build a ripple carry adder validator and manually fix the errors:
+  )
+
+(defn validate [g]
+  (let [gates (->> (ua/topsort g)
+                   (filter vector?)
+                   (remove (comp #{:init} first))
+                   (map (fn [[op :as node]]
+                          (let [preds (set (ug/predecessors g node))
+                                output (first (ug/successors g node))]
+                            [[op preds] output])))
+                   (into {}))]
+    (loop [idx 1
+           carry "nqp"]
+      (if (<= 45 idx)
+        ::its-a-good-adder
+        (let [fmt #(format "%s%02d" % idx)
+              in (into #{} (map fmt) ["x" "y"])
+              out (fmt "z")
+              mand (or (gates [:and in])
+                       (throw (ex-info "no mand" {:idx idx :carry carry})))
+              mxor (or (gates [:xor in])
+                       (throw (ex-info "no mxor" {:idx idx :carry carry})))
+              res (or (gates [:xor #{mxor carry}])
+                      (throw (ex-info "no res" {:idx idx :carry carry :mxor mxor :mand mand})))
+              _ (when-not (= out res)
+                  (throw (ex-info "res wrong" {:idx idx :carry carry :mxor mxor :mand mand :res res
+                                               :res-should-be out})))
+              mandxor (or (gates [:and #{carry mxor}])
+                          (throw (ex-info "no mandxor" {:idx idx :carry carry :mxor mxor})))
+              carryout (gates [:or #{mandxor mand}])]
+          (recur (inc idx) carryout))))))
+
+(comment
+  (validate g)
+  ;; iterate through the errors and fix them manually:
+
+  (-> g
+      (swap-output-wires "z06" "jmq")
+      (swap-output-wires "gmh" "z13")
+      (swap-output-wires "rqf" "cbd")
+      (swap-output-wires "qrh" "z38")
+      validate)
+  ;; => :hansbugge.year2024.day24/its-a-good-adder
+
+  (->> ["z06" "jmq"
+        "gmh" "z13"
+        "rqf" "cbd"
+        "qrh" "z38"]
+       sort
+       (str/join ","))
+  ;; => "cbd,gmh,jmq,qrh,rqf,z06,z13,z38"
   )
